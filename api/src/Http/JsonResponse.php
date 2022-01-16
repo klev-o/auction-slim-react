036ce4d575f449b7aab53a153ce4c0693aa23bd1
KonstantinLev <x-stels@bk.ru>
2022-01-16 14:27:36 +0300

    Added missed types

--- api/src/Http/JsonResponse.php
+++ api/src/Http/JsonResponse.php
@@ -10,6 +10,11 @@ use Slim\Psr7\Response;
 
 class JsonResponse extends Response
 {
+    /**
+     * @param mixed $data
+     * @param int $status
+     * @throws \JsonException
+     */
     public function __construct($data, int $status = 200)
     {
         parent::__construct(
