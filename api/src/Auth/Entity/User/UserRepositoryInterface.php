332974e2fac96868a4be1f64d4cf0e793cfe8aae
KonstantinLev <x-stels@bk.ru>
2022-02-10 11:01:21 +0300

    Implemented user repository

---
+++ api/src/Auth/Entity/User/UserRepositoryInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Auth\Entity\User;
+
+use DomainException;
+
+interface UserRepositoryInterface
+{
+    public function hasByEmail(Email $email): bool;
+    public function hasByNetwork(Network $network): bool;
+    public function findByJoinConfirmToken(string $token): ?User;
+    public function findByPasswordResetToken(string $token): ?User;
+    public function findByNewEmailToken(string $token): ?User;
+    /**
+     * @param Id $id
+     * @return User
+     * @throws DomainException
+     */
+    public function get(Id $id): User;
+    /**
+     * @param Email $email
+     * @return User
+     * @throws DomainException
+     */
+    public function getByEmail(Email $email): User;
+    public function add(User $user): void;
+    public function remove(User $user): void;
+}
