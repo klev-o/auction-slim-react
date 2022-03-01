054ad588d64916ea325b81db62197eee9146ce2f
KonstantinLev <x-stels@bk.ru>
2022-03-01 08:36:02 +0300

    Added tokenizer factory

--- api/config/common/auth.php
+++ api/config/common/auth.php
@@ -7,6 +7,7 @@ use App\Auth\Entity\User\User;
 use App\Auth\Entity\User\UserRepository;
 //use App\Auth\Service\JoinConfirmationSender;
 //use App\Frontend\FrontendUrlGenerator;
+use App\Auth\Service\Tokenizer;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Psr\Container\ContainerInterface;
@@ -19,6 +20,21 @@ return [
         $repo = $em->getRepository(User::class);
         return new UserRepository($em, $repo);
     },
+    Tokenizer::class => function (ContainerInterface $container): Tokenizer {
+        /**
+         * @psalm-suppress MixedArrayAccess
+         * @psalm-var array{token_ttl:string} $config
+         */
+        $config = $container->get('config')['auth'];
+
+        return new Tokenizer(new DateInterval($config['token_ttl']));
+    },
+
+    'config' => [
+        'auth' => [
+            'token_ttl' => 'PT1H',
+        ]
+    ],
 //    JoinConfirmationSender::class => function (ContainerInterface $container): JoinConfirmationSender {
 //        /** @var Swift_Mailer $mailer */
 //        $mailer = $container->get(Swift_Mailer::class);
