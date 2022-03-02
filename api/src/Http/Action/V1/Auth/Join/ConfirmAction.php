8a2eb57e1c8b13f51308f6b3a8cc0f04d2bb6a77
KonstantinLev <x-stels@bk.ru>
2022-03-02 10:37:24 +0300

    Extracted validation handling

--- api/src/Http/Action/V1/Auth/Join/ConfirmAction.php
+++ api/src/Http/Action/V1/Auth/Join/ConfirmAction.php
@@ -7,8 +7,6 @@ namespace App\Http\Action\V1\Auth\Join;
 use App\Auth\Command\JoinByEmail\Confirm\Command;
 use App\Auth\Command\JoinByEmail\Confirm\Handler;
 use App\Http\EmptyResponse;
-use App\Http\JsonResponse;
-use App\Http\Validator\ValidationException;
 use App\Http\Validator\Validator;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -36,16 +34,7 @@ class ConfirmAction implements RequestHandlerInterface
         $command = new Command();
         $command->token = $data['token'] ?? '';
 
-        try {
-            $this->validator->validate($command);
-        } catch (ValidationException $exception) {
-            $errors = [];
-            /** @var ConstraintViolationInterface $violation */
-            foreach ($exception->getViolations() as $violation) {
-                $errors[$violation->getPropertyPath()] = $violation->getMessage();
-            }
-            return new JsonResponse(['errors' => $errors], 422);
-        }
+        $this->validator->validate($command);
 
         $this->handler->handle($command);
 
