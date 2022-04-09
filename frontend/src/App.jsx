be31f39ebe61cd0b33e2655a7ff1e01307657ab5
KonstantinLev <x-stels@bk.ru>
2022-04-09 13:54:50 +0300

    Extracted welcome component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';;
 import './App.css';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="app">
-      <div className="welcome">
-        <h1>Auction Example</h1>
-        <p>It's working!</p>
-      </div>
+      <Welcome />
     </div>
   );
 }
---
+++ frontend/src/components/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Welcome.css';
+
+function Welcome() {
+  return (
+    <div className="welcome">
+      <h1>Auction Example</h1>
+      <p>It's Working!</p>
+    </div>
+  );
+}
+
+export default Welcome;
