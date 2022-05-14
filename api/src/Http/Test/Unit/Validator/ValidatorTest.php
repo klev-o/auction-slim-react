652a462080abeae758abbef6a2ceab7e682717dd
KonstantinLev <x-stels@bk.ru>
2022-05-14 11:36:27 +0300

    Changed expectations calls

--- api/src/Http/Test/Unit/Validator/ValidatorTest.php
+++ api/src/Http/Test/Unit/Validator/ValidatorTest.php
@@ -23,8 +23,8 @@ class ValidatorTest extends TestCase
         $command = new stdClass();
 
         $origin = $this->createMock(ValidatorInterface::class);
-        $origin->expects($this->once())->method('validate')
-            ->with($this->equalTo($command))
+        $origin->expects(self::once())->method('validate')
+            ->with(self::equalTo($command))
             ->willReturn(new ConstraintViolationList());
 
         $validator = new Validator($origin);
