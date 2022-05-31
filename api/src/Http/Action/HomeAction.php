8f342b70f4f14218f88eb54fb9814960614a3dcc
KonstantinLev <x-stels@bk.ru>
2022-05-31 10:37:00 +0300

    Added API feature toggle

--- api/src/Http/Action/HomeAction.php
+++ api/src/Http/Action/HomeAction.php
@@ -6,6 +6,7 @@ namespace App\Http\Action;
 
 //use App\Http;
 //use Psr\Http\Message\ResponseFactoryInterface;
+use App\FeatureToggle\FeatureFlag;
 use App\Http\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -14,10 +15,20 @@ use stdClass;
 
 class HomeAction implements RequestHandlerInterface
 {
+    private FeatureFlag $flag;
+
+    public function __construct(FeatureFlag $flag)
+    {
+        $this->flag = $flag;
+    }
+
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
 //        $response = $this->factory->createResponse();
 //        return Http::json($response, new stdClass());
+        if ($this->flag->isEnabled('NEW_HOME')) {
+            return new JsonResponse(['name' => 'API']);
+        }
         return new JsonResponse(new stdClass());
     }
 }
