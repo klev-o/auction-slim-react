7f615260fca18bb1ef00e55758b6748524d868ec
KonstantinLev <x-stels@bk.ru>
2022-05-31 10:29:57 +0300

    Added useFeatures hook

--- frontend/src/FeatureToggle/index.js
+++ frontend/src/FeatureToggle/index.js
@@ -1,4 +1,5 @@
 export { default as FeaturesContext } from './FeaturesContext'
 export { default as FeaturesProvider } from './FeaturesProvider'
 export { default as mergeFeatures } from './mergeFeatures'
+export { default as useFeatures } from './useFeatures'
 export { default } from './FeatureFlag'
