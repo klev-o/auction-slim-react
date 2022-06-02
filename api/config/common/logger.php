6debf76548b2f8d79a0f50b3a3a88e87cbfcd1d7
KonstantinLev <x-stels@bk.ru>
2022-06-03 00:52:57 +0300

    Added features logging

--- api/config/common/logger.php
+++ api/config/common/logger.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
+use App\FeatureToggle\FeaturesMonologProcessor;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
+use Monolog\Processor\ProcessorInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 
@@ -15,6 +17,7 @@ return [
          *     debug:bool,
          *     stderr:bool,
          *     file:string
+         *     processors:string[]
          * } $config
          */
         $config = $container->get('config')['logger'];
@@ -31,6 +34,12 @@ return [
             $log->pushHandler(new StreamHandler($config['file'], $level));
         }
 
+        foreach ($config['processors'] as $key => $class) {
+            /** @var ProcessorInterface $processor */
+            $processor = $container->get($class);
+            $log->pushProcessor($processor);
+        }
+
         return $log;
     },
 
@@ -39,6 +48,9 @@ return [
             'debug' => (bool)getenv('APP_DEBUG'),
             'file' => null,
             'stderr' => true,
+            'processors' => [
+                FeaturesMonologProcessor::class
+            ],
         ],
     ],
 ];
