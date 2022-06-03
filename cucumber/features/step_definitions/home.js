71b1d196f25bae82c8103e4b8ab33ff1a72d5c1d
KonstantinLev <x-stels@bk.ru>
2022-06-03 11:32:18 +0300

    Added common block step

--- cucumber/features/step_definitions/home.js
+++ cucumber/features/step_definitions/home.js
@@ -1,8 +1,9 @@
+//not used
 const { Then } = require('@cucumber/cucumber')
 // const { expect } = require('chai')
 
-Then('I see welcome block', async function () {
-  await this.page.waitForSelector('[data-testid=welcome]')
-  // const text = await this.page.$eval('[data-testid=welcome] h1', el => el.textContent)
-  // expect(text).to.eql('Auction Example')
-})
+// Then('I see welcome block', async function () {
+//   await this.page.waitForSelector('[data-testid=welcome]')
+//   // const text = await this.page.$eval('[data-testid=welcome] h1', el => el.textContent)
+//   // expect(text).to.eql('Auction Example')
+// })
