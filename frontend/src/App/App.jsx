4762bad09c21fe87ccc6d240f1ce196881b612ae
KonstantinLev <x-stels@bk.ru>
2022-06-15 14:36:09 +0300

    Added join page

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './App.css'
 import Home from '../Home'
+import Join from '../Join'
 import { NotFound } from '../Error'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { FeaturesProvider } from '../FeatureToggle'
@@ -13,6 +14,9 @@ function App({ features }) {
         <div className="app">
           <Routes>
             <Route exact path="/" element={<Home />} />
+            {features.includes('JOIN_TO_US') ? (
+              <Route exact path="/join" element={<Join />} />
+            ) : null}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
