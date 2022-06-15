105b5efaf2ff1bd189253e83e5f94890891693fb
KonstantinLev <x-stels@bk.ru>
2022-06-15 14:29:08 +0300

    Added Not Found

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './App.css'
 import Home from '../Home'
+import { NotFound } from '../Error'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { FeaturesProvider } from '../FeatureToggle'
 
@@ -12,6 +13,7 @@ function App({ features }) {
         <div className="app">
           <Routes>
             <Route exact path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </BrowserRouter>
