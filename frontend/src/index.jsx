85d6909e76b22b69b5be78c2992fedb30c4a76a2
KonstantinLev <x-stels@bk.ru>
2022-11-24 11:10:04 +0300

    Removed service workers

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './index.css'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
 import cookie from 'cookie'
 import defaultFeatures from './features'
 import { mergeFeatures } from './FeatureToggle'
@@ -20,8 +19,3 @@ root.render(
     <App features={features} />
   </React.StrictMode>
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
