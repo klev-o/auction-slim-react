592d1b1b7d47d189ec6ca169c7e17a1c2e7fe272
KonstantinLev <x-stels@bk.ru>
2022-12-09 20:43:16 +0300

    Updated Doctrine ORM

--- api/config/common/console.php
+++ api/config/common/console.php
@@ -5,9 +5,17 @@ declare(strict_types=1);
 use App\Console;
 use App\OAuth;
 use Doctrine\Migrations;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand;
+use Doctrine\ORM\Tools\Console\EntityManagerProvider;
+use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
+use Psr\Container\ContainerInterface;
 
 return [
+    EntityManagerProvider::class => static fn (ContainerInterface $container): EntityManagerProvider => new SingleManagerProvider($container->get(EntityManagerInterface::class)),
+
+    ValidateSchemaCommand::class => static fn (ContainerInterface $container): ValidateSchemaCommand => new ValidateSchemaCommand($container->get(EntityManagerProvider::class)),
+
     'config' => [
         'console' => [
             'commands' => [
